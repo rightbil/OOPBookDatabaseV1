a897fe0ff23c5944a88c92646beb4db6ff56c06d
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-03 13:28:02 -0500

    Book Application

---
+++ src/com/company/Book.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class Book {
+    private String author;
+    private String title;
+    private String description;
+
+    public Book(String author, String title, String description) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Book() {
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void getDistplayText() {
+
+        System.out.println("Author: " + author +
+                           "Title: " + title +
+                            "Descriptions: " + description);
+    }
+}
---
+++ src/com/company/BookApplication.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class BookApplication {
+
+    public static void main(String[] args) {
+
+        Book b= new Book("Alex","Ottello","a nice book");
+        b.getDistplayText();
+    }
+}
